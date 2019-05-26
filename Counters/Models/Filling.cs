8733b18d04d541f9328b656ade8eb21a8f86bf2c
zamo96 <zenyt2008>
2019-05-26 17:55:56 +0300

    ыва

--- Counters/Models/Filling.cs
+++ Counters/Models/Filling.cs
@@ -7,6 +7,7 @@ namespace Counters.Models
 {
     public class Filling
     {
+        // фафафаф
         public int Id { get; set; }
         public string Name { get; set; }
         public string Value { get; set; }
